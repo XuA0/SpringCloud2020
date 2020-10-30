97004a963e62909fa195b9de9c4148ac4e9a8827
Xu Ao <ao.xu@ncsi.com.cn>
2020-10-30 16:13:01 +0800

    GATE WAY

---
+++ cloud-gateway-gateway9527/src/main/java/com/xuao/springcloud/config/GateWayConfig.java
@@ -0,0 +1,19 @@
+package com.xuao.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder.Builder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class GateWayConfig {
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+		RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+		routes.route("path_route_xuao", 
+				r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+		return routes.build();
+	}
+}
